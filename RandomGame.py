a519bc7cf5d0e62ecfd9630864ee3565e87e630c
MitsuiWei <wei.xiang0702@gmail.com>
2021-07-11 12:47:33 +0800

    add count

--- RandomGame.py
+++ RandomGame.py
@@ -1,16 +1,23 @@
 import random
 r = random.randint(1,100)
+count = 0;
+while count < 3:
+	count = count + 1
 
-while True:
 	num = input("輸入數字1~100：")
 	num = int(num)
 
 	if num == r:
 		print("答對了!!!!!!!")
+		print("這是你猜的第", count ,"次")
 		break
 
 	elif num > r:
 		print("再小一點")
+		
 
 	elif num < r:
 		print("再大一點")
+		
+	
+	print("這是你猜的第", count ,"次")
