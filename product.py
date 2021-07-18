59314b8836b9189fde76cc529a04b2c7b1c06cfd
MitsuiWei <wei.xiang0702@gmail.com>
2021-07-18 23:54:54 +0800

    記帳程式

--- product.py
+++ product.py
@@ -1,50 +1,51 @@
 import os
 
 # 讀取檔案
-products = []
-if os.path.isfile('product.csv'):
-	with open('product.csv', 'r', encoding='utf8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf8') as f:
 		for line in f:
 			if '商品,價格' in line:
 				continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-	print(products)
-else:
-	print('找不到檔案！')
-
-
-
-
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	return products
 
 
 # 讓使用者輸入
-while True:
-	name = input('請輸入商品名稱：')
-	if name == 'q':
-		break
-	price = input('請輸入價格：')
-	# test = input('測試')
-
-	p = []
-	p.append(name)
-	p.append(price)
-	# p = [name, price] 等同於9,10,11行	
-
-	products.append(p) 
-	# 二維清單
-
-print(products)
-
-# print(products[0][0]) 
-# # 珍珠奶茶 , 0 -> 取到 珍珠奶茶
+def user_input(products):
+	while True:
+		name = input('請輸入商品名稱：')
+		if name == 'q':
+			break
+		price = input('請輸入價格：')
+		price = int(price)
+		products.append([name, price]) 
+		# 二維清單
+	return(products)
 
 # 印出所有購買紀錄
-for p in products:
-	print(p[0], '的價格', p[1])
+def print_products(products):
+	for p in products:
+		print(p[0], '的價格', p[1])
 
 # 寫入檔案
-with open('product.csv', 'w', encoding='utf8') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf8') as f:
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename):
+		print('找到檔案')
+		products = read_file(filename)
+	else:
+		print('找不到檔案！')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main()
